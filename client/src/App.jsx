d14062201cd11a286338f2946a5b806f7da358d8
Mansoor-P <munaf6007@gmail.com>
2024-11-27 09:22:04 +0530

    Completed front-end  implementation for Mansoor_VRV Security project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import FlipCard from './components/userLogin/userLogin';
+import ServicesCard from './components/services/service';
+import ProfileCard from './components/userProfile/userProfile';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(token);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>👮‍♀ VRV Security Service</h1>
+      <div className="panel">
+        {isAuthenticated ? <ProfileCard setIsAuthenticated={setIsAuthenticated} /> : <FlipCard setIsAuthenticated={setIsAuthenticated} />}
+        <ServicesCard />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
